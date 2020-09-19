c81659aed5f7f171a6a429133107fbf6e32747ad
neko-mkmt <ask1204@ztztnet.jp>
2020-09-19 17:53:47 +0900

    値の再計算

--- svelte-project/src/App.svelte
+++ svelte-project/src/App.svelte
@@ -1,10 +1,13 @@
 <script>
-	let name = '';
+	let firstName = '';
+	let lastName = '';
+	$: fullName = `${firstName} ${lastName}`
 </script>
 
 <main>
-	<input placeholder='Enter your name' bind:value={name}>
-	<div>Hello,{name}</div>
+	<input placeholder='Enter your name' bind:value={firstName}>
+	<input placeholder='Enter your name' bind:value={lastName}>
+	<div>Hello,{fullName}</div>
 </main>
 
 <style>
